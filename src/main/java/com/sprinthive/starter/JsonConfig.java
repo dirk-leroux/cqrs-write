51d7dd4cc30875da90ae63ad18fd00fc749bbec8
Jon Lai Lam <jon@lailam.co.za>
2017-11-30 15:10:49 +0200

    Configured the json object mapper so that it ignores nulls.

--- src/main/java/com/sprinthive/starter/JsonConfig.java
+++ src/main/java/com/sprinthive/starter/JsonConfig.java
@@ -1,5 +1,6 @@
 package com.sprinthive.starter;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -16,6 +17,7 @@ public class JsonConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         jsonConverter.setObjectMapper(objectMapper);
         return jsonConverter;
     }
